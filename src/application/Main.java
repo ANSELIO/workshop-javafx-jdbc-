cfffaf53e0885bf87a98b59ab33444e0857e9b4c
anselio jose <anselio@gmail.com>
2023-07-23 12:22:39 -0300

    Deleting .settings folder

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,15 +9,15 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-	private static Scene mainScene;
 	
+	private static Scene mainScene;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
+			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
@@ -28,12 +28,12 @@ public class Main extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-	}	
-	public static Scene getMainScene() {
-			return mainScene;
-		}
+	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
